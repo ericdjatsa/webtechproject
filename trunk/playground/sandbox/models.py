76827779b56e7853d6e1b9dc4a17d6727c0debbe
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-04-06 13:30:33 +0000

    moved timestamping to model

--- trunk/playground/sandbox/models.py
+++ trunk/playground/sandbox/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+# Contains the data model of a message
 class Message(models.Model):
 	title = models.CharField(max_length=30)
 	text = models.CharField(max_length=200)
 	timestamp = models.DateTimeField('Timestamp')
+
+	def save(self):
+		# on save, update the message timestamp
+		self.timestamp = datetime.now()
+		super(Message, self).save()
 
