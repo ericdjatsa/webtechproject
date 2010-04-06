d55c938eccb614d643ca08b7429fb513bf6954fa
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-04-06 13:16:12 +0000

    finished small message board project

--- trunk/playground/sandbox/models.py
+++ trunk/playground/sandbox/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+	title = models.CharField(max_length=30)
+	text = models.CharField(max_length=200)
+	timestamp = models.DateTimeField('Timestamp')
+
