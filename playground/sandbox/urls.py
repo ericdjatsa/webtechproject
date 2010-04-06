2e9ded7fc9217913f1006f65879cd73f6ba71694
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-04-06 13:16:12 +0000

    finished small message board project

--- playground/sandbox/urls.py
+++ playground/sandbox/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^sandbox/$', 'playground.sandbox.views.index'),
+    (r'^sandbox/post/$', 'playground.sandbox.views.post'),
+    (r'^sandbox/message/(?P<msg_id>\d+)/$', 'playground.sandbox.views.message'),
 #    (r'^polls/(?P<poll_id>\d+)/$', 'mysite.polls.views.detail'),
 #    (r'^polls/(?P<poll_id>\d+)/results/$', 'mysite.polls.views.results'),
 #    (r'^polls/(?P<poll_id>\d+)/vote/$', 'mysite.polls.views.vote'),
--- playground/sandbox/views.py
+++ playground/sandbox/views.py
@@ -1,6 +1,35 @@
 # Create your views here.
+from playground.sandbox.models import Message
+from django.template import Context, loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from datetime import datetime
+from django.shortcuts import get_object_or_404, render_to_response
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_message_list = Message.objects.all().order_by('-timestamp')[:5]
+    t = loader.get_template('sandbox/index.html')
+    c = Context({
+        'latest_message_list': latest_message_list,
+    })
+    return HttpResponse(t.render(c))
+
+
+def post(request):
+	try:
+		title = request.POST['title']
+		text = request.POST['text']
+		msg = Message(title = title, text = text, timestamp = datetime.now())
+		msg.save()		
+		return HttpResponseRedirect(reverse('playground.sandbox.views.index'))
+	except (KeyError):
+		t = loader.get_template('sandbox/post.html')
+		c = Context()
+		return HttpResponse(t.render(c))
+
+def message(request, msg_id):
+	msg = get_object_or_404(Message, pk=msg_id)
+	t = loader.get_template('sandbox/message.html')
+	c = Context({'message': msg,})
+	return HttpResponse(t.render(c))
