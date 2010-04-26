2be0674292043fe5b65789d2f983da68cbc5bd54
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-04-26 08:57:32 +0000

    started film subprojectwq

---
+++ playground/film/urls.py
@@ -0,0 +1,17 @@
+
+from django.conf.urls.defaults import *
+from playground.film.models import Film
+
+film_dict = {
+	'queryset' : Film.objects.all(), 
+	'paginate_by' : 24,
+}
+
+urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.list_detail.object_list', film_dict),
+    (r'^post/$', 'playground.film.views.post'),
+    (r'^message/(?P<msg_id>\d+)/$', 'playground.film.views.message'),
+#    (r'^polls/(?P<poll_id>\d+)/$', 'mysite.polls.views.detail'),
+#    (r'^polls/(?P<poll_id>\d+)/results/$', 'mysite.polls.views.results'),
+#    (r'^polls/(?P<poll_id>\d+)/vote/$', 'mysite.polls.views.vote'),
+)
