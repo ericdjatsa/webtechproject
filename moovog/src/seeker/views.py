c0d65eca78c2224ed426caa1b9360f2332d09bca
christophe.duhamel.0909 <christophe.duhamel.0909@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-04-29 09:29:41 +0000

    commit of my work on seeker : models + views + workflows

--- moovog/src/seeker/views.py
+++ moovog/src/seeker/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import Context, loader
+
+import logging
+from src.seeker.workflows import *
+from django.utils.simplejson import JSONDecoder, JSONEncoder
+
+def do_general_search(request):
+    wf = General_Search_WF(request.POST, None)
+    result = wf.work()
+    return render_to_response("template_targeted", result)
+    
+def do_specific_search(request):
+    wf = Specific_Search_WF(request.POST, None)
+    result = wf.work()
+    return render_to_response("template targeted", result)
+
+def create_fake_actor(request):
+    wf = Fake_Actor_Builder(JSONDecoder().decode(request.POST), None)
+    result = wf.work()
+    return HttpResponse(result["status"])
+
+def index(request):
+    return render_to_response('seeker/base.html')
