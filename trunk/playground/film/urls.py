e4c5df4f0602bd20035c85ade6ff70b8703f6b52
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-04-30 17:11:30 +0000

    added detail view for films

--- trunk/playground/film/urls.py
+++ trunk/playground/film/urls.py
@@ -7,10 +7,15 @@ film_dict = {
 	'paginate_by' : 5,
 }
 
+film_detail_dict = {
+	'queryset': Film.objects.all(),
+}
+
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.list_detail.object_list', film_dict),
 #    (r'^post/$', 'playground.film.views.post'),
     (r'^add_to_db/$', 'playground.film.views.add_to_db'),
+    (r'^movie/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', film_detail_dict),
 #    (r'^message/(?P<msg_id>\d+)/$', 'playground.film.views.message'),
 #    (r'^polls/(?P<poll_id>\d+)/$', 'mysite.polls.views.detail'),
 #    (r'^polls/(?P<poll_id>\d+)/results/$', 'mysite.polls.views.results'),
