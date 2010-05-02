f69b4fcc5276b7c7a7559fadc99fa4fd44ae7b82
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-05-02 15:56:15 +0000

    added frontend and crawler

---
+++ trunk/moovog/src/crawler/test_utils.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python2.6
+# -*-coding:Utf-8 -* 
+from utils import *
+print 'creating a new crawler'
+myC=crawler() 
+print u'myC start_time %s' % (myC.start_time)
+
+#starting the crawl job:
+
+myC.crawl()
+myC.saveToDB()
+
