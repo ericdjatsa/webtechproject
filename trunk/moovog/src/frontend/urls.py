862aa225c56edc0fb571a7df9df2ae9be89f28e7
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-05-18 13:33:29 +0000

    added settings page external combobox library

--- trunk/moovog/src/frontend/urls.py
+++ trunk/moovog/src/frontend/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 from src.frontend.models import Film
 
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
     (r'^$', 'src.frontend.views.index'),
     (r'^add_to_db/(?P<step>[1,2])/$', 'src.frontend.views.add_to_db'),
     (r'^movie/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', film_detail_dict),
+    (r'^preferences/$', 'src.frontend.views.preferences'),
 )
