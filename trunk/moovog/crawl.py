67e926ba8566bbdbeb4e4e6e2e755f12ddba9702
jonas.zaddach <jonas.zaddach@b746a600-2d66-11df-bfeb-7b4c664f286a>
2010-05-19 11:19:06 +0000

    added presentation hacks

--- trunk/moovog/crawl.py
+++ trunk/moovog/crawl.py
@@ -5,9 +5,16 @@ import settings
 from src.crawler.models import *
 from src.crawler.utils import *
 
+from src.frontend.models import Film
+
 setup_environ(settings)
 crawler = Crawler()
 crawler.crawl()
+
 File.objects.all().delete()
+
+#just for demo
+Film.objects.all().delete()
+
 crawler.saveToDB()
 
